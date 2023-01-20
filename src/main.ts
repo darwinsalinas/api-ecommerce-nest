88f99689047e908ed501f1e95b20de5766a4228b
Darwin Salinas <salinash2000@gmail.com>
2023-01-20 10:18:53 -0600

    configuraciones iniciales

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }
+  ));
+
+  const config = new DocumentBuilder()
+    .setTitle('API ecommerce using Nest.js')
+    .setDescription('Simple API ecommerce using Nest.js with TypeORM, and PostgreSQL')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
